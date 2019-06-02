f56a952a7bc71391acc496b6b50555c7dfbb0c69
typewriter <takuyaxp@gmail.com>
2019-06-02 16:34:20 +0900

    Remove unused variables (include invalidate apikey)

--- firebase2graph/src/plugins/firebase.js
+++ firebase2graph/src/plugins/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/firebase-database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBgKg-ffEYDrJlMhwS7ELNHhtsGpbDJ0E",
-  authDomain: "p2pquake.firebaseapp.com",
   databaseURL: "https://p2pquake.firebaseio.com",
-  projectId: "p2pquake",
-  storageBucket: "p2pquake.appspot.com",
-  messagingSenderId: "667662372160",
-  appId: "1:667662372160:web:c3b60ed307817bd3"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
